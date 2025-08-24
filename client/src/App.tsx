84a6057e2564efdbf086522c463afb4bc1d57f6d
TradersCorner <flavorgoodmedia@gmail.com>
2025-08-24 17:02:05 -0500

    Update App.tsx
    Signed-off-by: TradersCorner <flavorgoodmedia@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+// import { Toaster } from "@/components/ui/toaster";  // <-- remove
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
 import ThankYou from "@/pages/thank-you";
@@ -21,7 +21,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Toaster />
+        {/* <Toaster /> */} {/* removed to unblock build */}
         <Router />
       </TooltipProvider>
     </QueryClientProvider>
